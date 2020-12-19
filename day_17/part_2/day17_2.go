5bd263b474f25efbf1e6a6c4f5aea90ea10e0e6d
Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>
2020-12-19 14:47:14 +0100

    Day 17 - part 2

--- day_17/part_2/day17_2.go
+++ day_17/part_2/day17_2.go
@@ -13,32 +13,35 @@ type Cube struct {
 	X                  int
 	Y                  int
 	Z                  int
+	W                  int
 	Neighbors          []*Cube
 	AllocatedNeighbors int
 }
 
-func StringCoordinates(x, y, z int) string {
-	return fmt.Sprintf("%d,%d,%d", x, y, z)
+func StringCoordinates(x, y, z, w int) string {
+	return fmt.Sprintf("%d,%d,%d,%d", x, y, z, w)
 }
 
 func (cube *Cube) SetNeighbors(cubes map[string]*Cube, neighbors map[string]*Cube) {
 	var stringNeighborCoordinates string
 	var neighborCounter int = 0
 	cube.AllocatedNeighbors = 0
-	for z := -1; z <= 1; z++ {
-		for y := -1; y <= 1; y++ {
-			for x := -1; x <= 1; x++ {
-				if !(x == 0 && y == 0 && z == 0) {
-					stringNeighborCoordinates = StringCoordinates(cube.X+x, cube.Y+y, cube.Z+z)
-					if _, incubes := cubes[stringNeighborCoordinates]; !incubes {
-						if _, inneighbors := neighbors[stringNeighborCoordinates]; !inneighbors {
-							neighbor := Cube{false, cube.X + x, cube.Y + y, cube.Z + z, make([]*Cube, 26), 0}
-							cubes[stringNeighborCoordinates] = &neighbor
-							neighbors[stringNeighborCoordinates] = &neighbor
+	for w := -1; w <= 1; w++ {
+		for z := -1; z <= 1; z++ {
+			for y := -1; y <= 1; y++ {
+				for x := -1; x <= 1; x++ {
+					if !(x == 0 && y == 0 && z == 0 && w == 0) {
+						stringNeighborCoordinates = StringCoordinates(cube.X+x, cube.Y+y, cube.Z+z, cube.W+w)
+						if _, incubes := cubes[stringNeighborCoordinates]; !incubes {
+							if _, inneighbors := neighbors[stringNeighborCoordinates]; !inneighbors {
+								neighbor := Cube{false, cube.X + x, cube.Y + y, cube.Z + z, cube.W + w, make([]*Cube, 80), 0}
+								cubes[stringNeighborCoordinates] = &neighbor
+								neighbors[stringNeighborCoordinates] = &neighbor
+							}
 						}
+						cube.Neighbors[neighborCounter] = cubes[stringNeighborCoordinates]
+						neighborCounter++
 					}
-					cube.Neighbors[neighborCounter] = cubes[stringNeighborCoordinates]
-					neighborCounter++
 				}
 			}
 		}
@@ -59,7 +62,6 @@ func getCurrentCubes(cubes map[string]*Cube) []string {
 func processFile(filename string) map[string]*Cube {
 
 	cubes := make(map[string]*Cube)
-	//neighbors := make(map[string]*Cube)
 
 	var row int = 0
 
@@ -72,15 +74,14 @@ func processFile(filename string) map[string]*Cube {
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanLines)
 
-	// read map
 	for scanner.Scan() {
 		var column int = 0
 		for _, character := range scanner.Text() {
-			cube := Cube{false, column, row, 0, make([]*Cube, 26), 0}
+			cube := Cube{false, column, row, 0, 0, make([]*Cube, 80), 0}
 			if character == '#' { // Active
 				cube.Active = true
 			}
-			cubes[StringCoordinates(cube.X, cube.Y, cube.Z)] = &cube
+			cubes[StringCoordinates(cube.X, cube.Y, cube.Z, cube.W)] = &cube
 			column++
 		}
 		row++
@@ -106,7 +107,7 @@ func runCicles(cubes map[string]*Cube, cicles int) map[string]*Cube {
 
 		for _, cube := range cubes {
 			var activeCubes int = 0
-			var cubeStringCoordinates = StringCoordinates(cube.X, cube.Y, cube.Z)
+			var cubeStringCoordinates = StringCoordinates(cube.X, cube.Y, cube.Z, cube.W)
 			newStates[cubeStringCoordinates] = cube.Active
 			for neighbor := 0; neighbor < cube.AllocatedNeighbors; neighbor++ {
 				if cube.Neighbors[neighbor].Active == true {
@@ -115,10 +116,8 @@ func runCicles(cubes map[string]*Cube, cicles int) map[string]*Cube {
 			}
 
 			if cube.Active {
-				//	fmt.Println("Cube ", cubeStringCoordinates, "has ", activeCubes, "active neighbors")
 				if activeCubes != 2 && activeCubes != 3 {
 					newStates[cubeStringCoordinates] = false
-					//	stateChanged = true
 				} else {
 					newStates[cubeStringCoordinates] = true
 				}
@@ -130,7 +129,7 @@ func runCicles(cubes map[string]*Cube, cicles int) map[string]*Cube {
 					newStates[cubeStringCoordinates] = false
 				}
 			}
-			if cube.AllocatedNeighbors != 26 {
+			if cube.AllocatedNeighbors != 80 {
 				cube.SetNeighbors(cubes, neighbors)
 			}
 		}
