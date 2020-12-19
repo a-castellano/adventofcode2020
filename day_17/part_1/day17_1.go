671f0cf1079f299da575a671743c0e39ce254928
Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>
2020-12-19 10:38:46 +0100

    Day 17, part 1

--- day_17/part_1/day17_1.go
+++ day_17/part_1/day17_1.go
@@ -1,4 +1,4 @@
-// Álvaro Castellano Vela 2020/12/17
+// Álvaro Castellano Vela 2020/12/19
 package main
 
 import (
@@ -6,52 +6,79 @@ import (
 	"fmt"
 	"log"
 	"os"
-	//"strings"
 )
 
 type Cube struct {
-	Active    bool
-	X         int
-	Y         int
-	Z         int
-	Neighbors []*Cube
+	Active             bool
+	X                  int
+	Y                  int
+	Z                  int
+	Neighbors          []*Cube
+	AllocatedNeighbors int
 }
 
-func (cube *Cube) SetNeighbors(cubes map[string]*Cube, neighbors map[string]map[string]*Cube) {
-	var cubeStringCoordinates string = StringCoordinates(cube.X, cube.Y, cube.Z)
+func StringCoordinates(x, y, z int) string {
+	return fmt.Sprintf("%d,%d,%d", x, y, z)
+}
+
+func (cube *Cube) SetPartialNeighbors(cubes map[string]*Cube) {
 	var stringNeighborCoordinates string
-	cubeNeighbors := make(map[string]*Cube)
-	count := 0
+	var neighborCounter int = 0
 	for z := -1; z <= 1; z++ {
 		for y := -1; y <= 1; y++ {
 			for x := -1; x <= 1; x++ {
-				stringNeighborCoordinates = StringCoordinates(cube.X+x, cube.Y+y, cube.Z+z)
-				if stringNeighborCoordinates != cubeStringCoordinates {
-					var neighbor Cube
-					if _, ok := cubes[stringNeighborCoordinates]; !ok {
-						neighbor.X = x
-						neighbor.Y = y
-						neighbor.Z = z
-						neighbor.Active = false
+				if !(x == 0 && y == 0 && z == 0) {
+					stringNeighborCoordinates = StringCoordinates(cube.X+x, cube.Y+y, cube.Z+z)
+					if _, ok := cubes[stringNeighborCoordinates]; ok {
+						cube.Neighbors[neighborCounter] = cubes[stringNeighborCoordinates]
+						neighborCounter++
 					}
-					cubeNeighbors[stringNeighborCoordinates] = cubes[stringNeighborCoordinates]
-					count++
 				}
 			}
 		}
 	}
-	neighbors[cubeStringCoordinates] = cubeNeighbors
-	cube.HasNeighbors = true
+	cube.AllocatedNeighbors = neighborCounter
 }
 
-func StringCoordinates(X, Y, Z int) string {
-	return fmt.Sprintf("%d,%d,%d", X, Y, Z)
+func (cube *Cube) SetNeighbors(cubes map[string]*Cube, neighbors map[string]*Cube) {
+	var stringNeighborCoordinates string
+	var neighborCounter int = 0
+	cube.AllocatedNeighbors = 0
+	for z := -1; z <= 1; z++ {
+		for y := -1; y <= 1; y++ {
+			for x := -1; x <= 1; x++ {
+				if !(x == 0 && y == 0 && z == 0) {
+					stringNeighborCoordinates = StringCoordinates(cube.X+x, cube.Y+y, cube.Z+z)
+					if _, incubes := cubes[stringNeighborCoordinates]; !incubes {
+						if _, inneighbors := neighbors[stringNeighborCoordinates]; !inneighbors {
+							neighbor := Cube{false, cube.X + x, cube.Y + y, cube.Z + z, make([]*Cube, 26), 0}
+							cubes[stringNeighborCoordinates] = &neighbor
+							neighbors[stringNeighborCoordinates] = &neighbor
+						}
+					}
+					cube.Neighbors[neighborCounter] = cubes[stringNeighborCoordinates]
+					neighborCounter++
+				}
+			}
+		}
+	}
+	cube.AllocatedNeighbors = neighborCounter
 }
 
-func processFile(filename string) (map[string]*Cube, map[string]map[string]*Cube) {
+func getCurrentCubes(cubes map[string]*Cube) []string {
+	cubeList := make([]string, len(cubes))
+	var counter int = 0
+	for stringCoordinates, _ := range cubes {
+		cubeList[counter] = stringCoordinates
+		counter++
+	}
+	return cubeList
+}
+
+func processFile(filename string) map[string]*Cube {
 
 	cubes := make(map[string]*Cube)
-	neighbors := make(map[string]map[string]*Cube)
+	neighbors := make(map[string]*Cube)
 
 	var row int = 0
 
@@ -68,7 +95,7 @@ func processFile(filename string) (map[string]*Cube, map[string]map[string]*Cube
 	for scanner.Scan() {
 		var column int = 0
 		for _, character := range scanner.Text() {
-			cube := Cube{false, column, row, 0, false}
+			cube := Cube{false, column, row, 0, make([]*Cube, 26), 0}
 			if character == '#' { // Active
 				cube.Active = true
 			}
@@ -78,60 +105,68 @@ func processFile(filename string) (map[string]*Cube, map[string]map[string]*Cube
 		row++
 	}
 
-	fmt.Println(len(cubes))
-
-	for _, cube := range cubes {
-		cube.SetNeighbors(cubes, neighbors)
+	counter := 0
+	currentCubes := getCurrentCubes(cubes)
+	for _, stringCoordinates := range currentCubes {
+		cubes[stringCoordinates].SetNeighbors(cubes, neighbors)
+		counter++
+	}
+	currentNeighbors := getCurrentCubes(neighbors)
+	for _, stringCoordinates := range currentNeighbors {
+		neighbors[stringCoordinates].SetNeighbors(cubes, neighbors)
 	}
 
-	return cubes, neighbors
+	return cubes
 }
 
-func runCicles(cubes map[string]*Cube, neighbors map[string]map[string]*Cube, cicles int) {
-	newStates := make(map[string]bool)
-	for _, cube := range cubes {
-		var activeCubes int = 0
-		var stateChanged bool = false
-		var cubeStringCoordinates = StringCoordinates(cube.X, cube.Y, cube.Z)
-		newStates[cubeStringCoordinates] = cube.Active
-		for _, neighbor := range neighbors[cubeStringCoordinates] {
-			if neighbor.Active == true {
-				activeCubes++
+func runCicles(cubes map[string]*Cube, cicles int) {
+	for cicle := 0; cicle < cicles; cicle++ {
+		newStates := make(map[string]bool)
+		neighbors := make(map[string]*Cube)
+		for _, cube := range cubes {
+			var activeCubes int = 0
+			var cubeStringCoordinates = StringCoordinates(cube.X, cube.Y, cube.Z)
+			newStates[cubeStringCoordinates] = cube.Active
+			for neighbor := 0; neighbor < cube.AllocatedNeighbors; neighbor++ {
+				if cube.Neighbors[neighbor].Active == true {
+					activeCubes++
+				}
 			}
 
+			if cube.Active {
+				//	fmt.Println("Cube ", cubeStringCoordinates, "has ", activeCubes, "active neighbors")
+				if activeCubes != 2 && activeCubes != 3 {
+					newStates[cubeStringCoordinates] = false
+					//	stateChanged = true
+				} else {
+					newStates[cubeStringCoordinates] = true
+				}
+			} else {
+				if cube.Active == false && activeCubes == 3 {
+					newStates[cubeStringCoordinates] = true
+					//		stateChanged = true
+				} else {
+					newStates[cubeStringCoordinates] = false
+				}
+			}
+			if cube.AllocatedNeighbors != 26 {
+				cube.SetNeighbors(cubes, neighbors)
+			}
+		}
+		for cubeStringCoordinates, cube := range cubes {
+			cube.Active = newStates[cubeStringCoordinates]
+		}
+		currentNeighbors := getCurrentCubes(neighbors)
+		for _, stringCoordinates := range currentNeighbors {
+			neighbors[stringCoordinates].SetNeighbors(cubes, neighbors)
 		}
 
-		if cube.Active {
-			fmt.Println("Cube ", cubeStringCoordinates, "has ", activeCubes, "active neighbors")
-			if activeCubes != 2 && activeCubes != 3 {
-				newStates[cubeStringCoordinates] = false
-				stateChanged = true
-			} else {
-				newStates[cubeStringCoordinates] = true
-			}
-		} else {
-			if cube.Active == false && activeCubes == 3 {
-				newStates[cubeStringCoordinates] = true
-				stateChanged = true
-			} else {
-				newStates[cubeStringCoordinates] = false
-			}
-		}
-		if stateChanged && cube.HasNeighbors == false {
-			fmt.Println("Changing Neighbors of", cube)
-			cube.SetNeighbors(cubes, neighbors)
-			fmt.Println("after Neighbors of", cube)
-		}
-	}
-	for cubeStringCoordinates, _ := range cubes {
-		cubes[cubeStringCoordinates].Active = newStates[cubeStringCoordinates]
 	}
 }
 
 func countActive(cubes map[string]*Cube) int {
 	var activeCubes int = 0
 	for _, cube := range cubes {
-		fmt.Println(cube)
 		if cube.Active {
 			activeCubes++
 		}
@@ -146,16 +181,7 @@ func main() {
 	}
 	filename := args[0]
 
-	cubes, neighbors := processFile(filename)
-	fmt.Println("Cubes", cubes)
-	fmt.Println("Neighbors", neighbors)
-	fmt.Println(len(cubes))
-	fmt.Println("activeCubes:", countActive(cubes))
-	fmt.Println("__________")
-	runCicles(cubes, neighbors, 1)
-	fmt.Println("Cubes", cubes)
-	fmt.Println("Neighbors", neighbors)
-	fmt.Println("__________")
-	fmt.Println(len(cubes))
-	fmt.Println("activeCubes:", countActive(cubes))
+	cubes := processFile(filename)
+	runCicles(cubes, 6)
+	fmt.Println("Active Cubes:", countActive(cubes))
 }
